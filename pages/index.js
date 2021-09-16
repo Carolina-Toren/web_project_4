3ea2a4111ae9f6611888bdd25ce8b130393ac39a
Carolina Toren <54749413+Carolina-Toren@users.noreply.github.com>
2021-09-16 09:58:47 +0300

    popup window input values

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
-let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
-let closeButton = document.querySelector(".popup__close-btn");
+let popupSubmitButton = popup.querySelector(".popup__save-button");
+let closeButton = popup.querySelector(".popup__close-btn");
+let inputFullName = popup.querySelector(".popup__fullName");
+let inputOccupation = popup.querySelector(".popup__occupation");
+
+let editButton = document.querySelector(".profile__edit-button");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__occupation");
 
 function openPopup() {
   popup.classList.add("popup_visible");
 }
-
-editButton.addEventListener("click", openPopup);
 
 function closePopup() {
   popup.classList.remove("popup_visible");
 }
 
 closeButton.addEventListener("click", closePopup);
+
+editButton.addEventListener("click", () => {
+  inputFullName.value = profileName.textContent;
+  inputOccupation.value = profileJob.textContent;
+  openPopup();
+});
